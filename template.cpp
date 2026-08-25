505a9ad05c73842dd6794a55da40ab3c8bfc2590
Makhzum-Bin-Harun <76556113+makhzumbinharun@users.noreply.github.com>
2026-08-26 01:30:31 +0600

    Create template.cpp

---
+++ template.cpp
@@ -0,0 +1,16 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define ll long long
+#define pb push_back
+#define all(x) (x).begin(), (x).end()
+const ll MOD = 1e9+7, INF = 4e18;
+#define f0(n) for(int i = 0; i < n; i++);
+#define f1(n) for(int i = 1; i < n; i++);
+
+int main(){
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
+    return 0;
+}
