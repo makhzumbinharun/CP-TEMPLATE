62594b002c04c56909992af54df5150042df34b1
Makhzum-Bin-Harun <76556113+makhzumbinharun@users.noreply.github.com>
2026-09-21 23:26:06 +0600

    Update template.cpp

--- template.cpp
+++ template.cpp
@@ -2,15 +2,15 @@
 using namespace std;
 #define ll long long
 #define pb push_back
+#define el '\n'
+#define fast ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL);
 #define all(x) (x).begin(), (x).end()
 const ll MOD = 1e9+7, INF = 4e18;
-#define f0(n) for(int i = 0; i < n; i++);
+#define f0(n) for(int i = 0; i < n; i++)
 #define f1(n) for(int i = 1; i < n; i++);
 
 int main(){
-    ios_base::sync_with_stdio(false);
-    cin.tie(NULL);
-    cout.tie(NULL);
-
+    fast
+    
     return 0;
 }
